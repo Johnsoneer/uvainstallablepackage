b4a548668a6f9d4f35928cfd7ddab2265feeaee6
Will Johnson <will@Wills-MBP-2.lan>
2023-02-05 20:44:45 -0600

    Lab-2: Adding lab python and requirements files to my fork of uvainstallablepackage

---
+++ lab_2/test_shared.py
@@ -0,0 +1,27 @@
+import shared as sh
+import re
+sh.afunction()
+
+
+def space_compress(str_in:str):
+	"""
+	Take a spring and return a string a string with no whitespaces
+
+	Args:
+		- str_in(str): string to remove whitespace
+	"""
+	if type(str_in) != str:
+		return "Expected string but got {}".format(dtype(str_in))
+
+	else:
+		# replace multiline string with one-line string
+		str_out = str_in.replace('\n',"")
+
+		# reduce multiple whitespaces into one white space
+		str_out = " ".join(str_in.split())
+
+		return str_out
+
+# if __name__=="__main__":
+# 	txt = input("test inputs: ")
+# 	print(space_compress(txt))
